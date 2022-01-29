a404e3c1af27ed79884e4644ff54393f0e2d72bb
g30rgee <90869563+g30rgeee@users.noreply.github.com>
2022-01-29 14:29:29 +0100

    the end

--- Hotel Management System Project.cpp
+++ Hotel Management System Project.cpp
@@ -29,12 +29,13 @@ int main()
     cout << "\n Quantity of chicken-roll: ";
     cin >> Qchicken;
 
+    m:
     cout << "\n\t\t\t Please select from the menu options ";
     cout << "n\n1) Rooms";
     cout << "\n2 ) Pasta";
     cout << "\n3 ) Burger";
-    cout << "\n4 ) ";
-    cout << "\n5 ) shake";
+    cout << "\n4 ) Noodles";
+    cout << "\n5 ) Shake";
     cout << "\n6 ) Chicken-roll";
     cout << "\n7 ) Information regarding sales and collection ";
     cout << "\n8 ) Exit";
@@ -61,20 +62,132 @@ int main()
             break;
 
         case 2:
-            cout << "\n\n Enter the number of rooms you want: ";
+            cout << "\n\n Enter pasta quantity: ";
 
             cin >> quant;
-            if (Qrooms - Srooms >= quant)
+            if (Qpasta - Spasta >= quant)
             {
-                Srooms = Srooms + quant;
-                Total_room = (Total_room+quant*1200);
-                cout << "\n\n\t\t" << quant << "room/rooms have been allowed to you! ";
+                Spasta = Spasta + quant;
+                Total_pasta = (Total_pasta+quant*250);
+                cout << "\n\n\t\t" << quant << "pasta is the order! ";
             }
             else
 
             {
-                cout << "\n\tOnly" << Qrooms - Srooms << "Rooms remaining in hotel ";
+                cout << "\n\tOnly" << Qpasta - Spasta << "Pasta remaining in hotel ";
                 break;
             }
+
+        case 3:
+            cout << "\n\n Enter burger quantity: ";
+
+            cin >> quant;
+            if (Qburger - Sburger >= quant)
+            {
+                Sburger = Sburger + quant;
+                Total_burger = (Total_burger+quant*120);
+                cout << "\n\n\t\t" << quant << "burger is the order! ";
+            }
+            else
+
+            {
+                cout << "\n\tOnly" << Qburger - Sburger << "Burger remaining in hotel ";
+                break;
+            }
+
+        case 4:
+            cout << "\n\n Enter noodles quantity: ";
+
+            cin >> quant;
+            if (Qnoodles - Snoodles >= quant)
+            {
+                Snoodles = Snoodles + quant;
+                Total_noodles = (Total_noodles+quant*140);
+                cout << "\n\n\t\t" << quant << "noodles is the order! ";
+            }
+            else
+
+            {
+                cout << "\n\tOnly" << Qnoodles - Snoodles << "Noodles remaining in hotel ";
+                break;
+            }
+
+        case 5:
+            cout << "\n\n Enter shake quantity: ";
+
+            cin >> quant;
+            if (Qshake - Sshake >= quant)
+            {
+                Sshake = Sshake + quant;
+                Total_shake = (Total_shake+quant*120);
+                cout << "\n\n\t\t" << quant << "shake is the order! ";
+            }
+            else
+
+            {
+                cout << "\n\tOnly" << Qshake - Sshake << "Chake remaining in hotel ";
+                break;
+            }
+
+        case 6:
+            cout << "\n\n Enter chicken-roll quantity: ";
+
+            cin >> quant;
+            if (Qchicken - Schicken >= quant)
+            {
+                Schicken = Schicken + quant;
+                Total_chicken = (Total_chicken+quant*150);
+                cout << "\n\n\t\t" << quant << "chicken is the order! ";
+            }
+            else
+
+            {
+                cout << "\n\tOnly" << Qchicken - Schicken << "Chicken-roll remaining in hotel ";
+                break;
+            }
+
+        case 7:
+            cout << "\n\t\tDetails of sales and collection ";
+            cout << "\n\n Number of room we had : " << Qrooms;
+            cout << "\n\n Number of rooms we gave for rent : " << Srooms;
+            cout << "\n\n Reamining rooms : " << Qrooms - Srooms;
+            cout << "\n\n Total rooms collection for the day : " << Total_room;
+
+            cout << "\n\t\tDetails of Pastas and collection ";
+            cout << "\n\n Number of pastas we had : " << Qpasta;
+            cout << "\n\n Number of pastas we gave for rent : " << Spasta;
+            cout << "\n\n Reamining pastas : " << Qpasta - Spasta;
+            cout << "\n\n Total pasta collection for the day : " << Total_pasta;
+
+            cout << "\n\t\tDetails of burger and collection ";
+            cout << "\n\n Number of burgers we had : " << Qburger;
+            cout << "\n\n Number of burgers we gave for rent : " << Sburger;
+            cout << "\n\n Reamining burgers : " << Qburger - Sburger;
+            cout << "\n\n Total burgers collection for the day : " << Total_burger;
+
+            cout << "\n\t\tDetails of noodles and collection ";
+            cout << "\n\n Number of noodles we had : " << Qnoodles;
+            cout << "\n\n Number of noodles we gave for rent : " << Snoodles;
+            cout << "\n\n Reamining noodles : " << Qnoodles - Snoodles;
+            cout << "\n\n Total noodles collection for the day : " << Total_noodles;
+
+            cout << "\n\t\tDetails of shakes and collection ";
+            cout << "\n\n Number of shakes we had : " << Qshake;
+            cout << "\n\n Number of shakes we gave for rent : " << Sshake;
+            cout << "\n\n Reamining shakes : " << Qshake - Sshake;
+            cout << "\n\n Total shakes collection for the day : " << Total_shake;
+
+            cout << "\n\t\tDetails of chicken-roll and collection ";
+            cout << "\n\n Number of chicken-roll we had : " << Qchicken;
+            cout << "\n\n Number of chicken-roll we gave for rent : " << Schicken;
+            cout << "\n\n Reamining chicken-roll : " << Qchicken - Schicken;
+            cout << "\n\n Total chicken-roll collection for the day : " << Total_chicken;
+        
+            case 8:
+            exit(0);
+
+            default:
+                cout << "\n Please select the numbers mentioned above! ";
         }
+        goto m;
     }
